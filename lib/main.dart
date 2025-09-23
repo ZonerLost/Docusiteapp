5464bed3e09fcde73b828b067700ea3fe265cd33
Waleed Ahmad <34332682+Khushnu@users.noreply.github.com>
2025-09-23 17:08:32 +0500

    fixes minor

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
   runApp(MyApp());
 }
 
-// DO NOT REMOVE Unless you find their usage.
+
 String dummyImg =
     'https://images.unsplash.com/photo-1558507652-2d9626c4e67a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80';
 
